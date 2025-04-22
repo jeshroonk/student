eee624042c24291ffda09542cd8e56316d3d5d03
veeres-javali18 <sharatjoshi1@gmail.com>
2025-04-23 03:24:21 +0530

    save

---
+++ D.java
@@ -0,0 +1,31 @@
+public class D{
+    public static void main(String[] args) {
+       
+        int age = 25;
+        System.out.println("Age: " + age);
+
+       
+        double price = 99.99;
+        System.out.println("Price: " + price);
+
+        
+        char grade = 'A';
+        System.out.println("Grade: " + grade);
+
+        
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " + isJavaFun);
+
+        
+        String greeting = "Hello, Java!";
+        System.out.println("Greeting: " + greeting);
+
+       
+        long distance = 9876543210L;
+        System.out.println("Distance: " + distance);
+
+        
+        float temperature = 36.6f;
+        System.out.println("Temperature: " + temperature);
+    }
+}
