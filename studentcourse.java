2d43a0bc2ece837259bd403955e1b7f3b883afac
veeres-javali18 <sharatjoshi1@gmail.com>
2025-05-07 06:34:38 +0530

    add

---
+++ studentcourse.java
@@ -0,0 +1,15 @@
+public class studentcourse{
+    public static void main(String[] args){
+    String name = "jesh";
+    int srn=014;
+    String course="devops";
+    int marks=89;
+
+    System.out.println("Name:" + name);
+    System.out.println("Srn:" + srn);
+    System.out.println("Course:" + course);
+    System.out.println("Marks:" + marks);
+    }
+
+
+}
